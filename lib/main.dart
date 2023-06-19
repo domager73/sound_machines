8f297bb973550b88b84c62e48bdd6504c0d2b824
Jason1258t <ivandomogatskij@gmail.com>
2023-06-19 13:29:16 +0300

    feature

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'firebase_options.dart';
 
