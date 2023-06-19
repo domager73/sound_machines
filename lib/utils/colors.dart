5754c831da4762a99ff62a2e753a9f70542c5406
domager73 <112970685+domager73@users.noreply.github.com>
2023-06-19 16:47:20 +0300

    add login_screen.dart

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -4,4 +4,6 @@ abstract class AppColors {
   static const blackColor = Color(0xff121212);
   static const greenButton = Colors.green;
   static const borderGrey = Colors.grey;
+  static const errorColorTextField = Color(0xffD9D9D9);
+  static const colorTextField = Color(0xff414141);
 }
